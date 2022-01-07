02f244777012380c627685f9d4bc70821ca0df95
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2022-01-07 11:53:14 +0100

    Created function to check if cards are matched or not

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import {useState, useEffect} from 'react';
 import SingleCard from './components/SingleCard'
 
 const cardImages = [
-  { "src" : "/img/helmet-1.png" },
-  { "src" : "/img/potion-1.png" },
-  { "src" : "/img/ring-1.png" },
-  { "src" : "/img/scroll-1.png" },
-  { "src" : "/img/shield-1.png" },
-  { "src" : "/img/sword-1.png" }
+  { "src" : "/img/potion-1.png", matched : false},
+  { "src" : "/img/helmet-1.png", matched : false},
+  { "src" : "/img/ring-1.png", matched : false},
+  { "src" : "/img/scroll-1.png", matched : false},
+  { "src" : "/img/shield-1.png", matched : false},
+  { "src" : "/img/sword-1.png", matched : false}
 ]
 
 function App() {
@@ -39,14 +39,23 @@ function App() {
     if (choiceOne && choiceTwo){
 
       if (choiceOne.src === choiceTwo.src){
-        console.log("Those Cards match")
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return {...card, matched: true}
+            }else{
+              return card
+            }
+          })
+        })
         resetTurn()
       } else{
-        console.log("Those card do not match")
         resetTurn()
       }
     }
   }, [choiceOne, choiceTwo])
+
+  console.log([cards])
 
   //Reset choices & increase turn
   const resetTurn = () => {
